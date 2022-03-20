6da81189cb6526de6e3d30b5b3ecf1bb247cf990
karlG <kallegunnarsson@hotmail.com>
2022-03-20 13:33:09 +0100

    new if structure

--- server.js
+++ server.js
@@ -2,15 +2,25 @@ const http = require('http')
 const { getProducts, getProduct } = require('./controllers/productController')
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/api/products' && req.method === 'GET') {
-        getProducts(req, res)
-    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
-        const id = req.url.split('/')[3]
-        getProduct(req, res, id)
-    } 
-    else {
+
+    function notFound() {
         res.writeHead(404, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify({ message: 'route not found' }))
+    }
+
+    switch (req.method) {
+        case 'GET':
+            req.url === '/api/products' ? getProducts(req, res)
+
+                : req.url.match(/\/api\/products\/([0-9]+)/) ? getProduct(req, res, req.url.split('/')[3])
+
+                    : notFound()
+            break
+        case 'POST':
+            // create product
+            break
+        default:
+            console.log('not a valid request method')
     }
 
 })
